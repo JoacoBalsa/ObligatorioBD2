9025028819f1b2412e3bc989401bd69685bebddd
JoacoBalsa <115119466+JoacoBalsa@users.noreply.github.com>
2023-04-18 10:42:55 -0300

    Update Ejercicio_1.sql

--- Ejercicio_1.sql
+++ Ejercicio_1.sql
@@ -1,6 +1,6 @@
 select m.name, m.runtime, m.vote_average, YEAR(m.date) as year
 from movies as m
-where m.date >= '2011-01-01'
+where m.date >= '2011-01-01' and m.date < now()
 order by YEAR(m.date) asc;
 
 
